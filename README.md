5402920314b2e91156fcd2cc4e08b15cb163c0f8
tkachenko-maksym <tkachenko.maksym@student.sumdu.edu.ua>
2024-12-02 00:58:44 +0200

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+<p align="center">
+    <a href="https://github.com/yiisoft" target="_blank">
+        <img src="https://avatars0.githubusercontent.com/u/993323" height="100px">
+    </a>
+    <h1 align="center">Курсова робота з дисципліни «Проектування веб-орієнтованих інформаційних систем»</h1>
+    <br>
+   <h2 align="center">Ткаченко Максим ІТ.м-42</h2>
+</p>
---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql;dbname=blog_db',
+    'username' => 'root',
+    'password' => 'rootpassword',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
